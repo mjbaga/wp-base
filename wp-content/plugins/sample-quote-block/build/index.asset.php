05aad822edb6e58d2d8cd1badbf652609cd69804
Marvin Baga <mjdbaga@gmail.com>
2023-09-06 19:33:00 +0800

    feat wip: add base plugin quote

---
+++ wp-content/plugins/sample-quote-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3e54e10a41ac37e10f85');
---
+++ wp-content/plugins/sample-quote-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6ae61cb01216159df2b7');
